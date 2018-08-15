aa7a1c4db0f2e3e2762331d62ec46db77c73f132
machbase <info@machbase.com>
2018-08-15 11:22:28 +0900

    modi

--- edu_2_plc/4_plc_tag_load.sh
+++ edu_2_plc/4_plc_tag_load.sh
@@ -1,3 +1,3 @@
 
-machloader  -t plc_tag_table -i -H -d 4_plc_tag.csv -F "tm YYYY-MM-DD HH24:MI:SS mmm:uuu:nnn"
+machloader  -t plc_tag_table -i -d 4_plc_tag.csv -F "tm YYYY-MM-DD HH24:MI:SS mmm:uuu:nnn"
 
