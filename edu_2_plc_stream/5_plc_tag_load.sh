f2a1428d494ce8d03d66e460e63f0a07e5e748ff
machbase <info@machbase.com>
2018-08-15 12:55:04 +0900

    local commit

--- edu_2_plc_stream/5_plc_tag_load.sh
+++ edu_2_plc_stream/5_plc_tag_load.sh
@@ -1,3 +1,3 @@
 
-machloader  -t plc_tag_table -i -H -d 5_plc_tag.csv -F "tm YYYY-MM-DD HH24:MI:SS mmm:uuu:nnn"
+machloader  -t plc_tag_table -i -d 5_plc_tag.csv -F "tm YYYY-MM-DD HH24:MI:SS mmm:uuu:nnn"
 
