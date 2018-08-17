98cab9ec13f40217236bfba44a2ed0ece78c8081
machbase <info@machbase.com>
2018-08-17 14:19:55 +0900

    Removed folder from repository

--- edu_1_basic/2_insert_meta.sql
+++
@@ -1,2 +0,0 @@
-insert into tag metadata values ('MTAG_C00');
-insert into tag metadata values ('MTAG_C01');
--- edu_1_basic/3_load.sh
+++
@@ -1,2 +0,0 @@
-csvimport -t TAG -d 3_MTAG_C00.csv -F "time YYYY-MM-DD HH24:MI:SS mmm:uuu:nnn" -l error.log
-csvimport -t TAG -d 3_MTAG_C01.csv -F "time YYYY-MM-DD HH24:MI:SS mmm:uuu:nnn" -l error.log
